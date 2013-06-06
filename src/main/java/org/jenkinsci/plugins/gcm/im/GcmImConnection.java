e3ee556f2dedf535d6aaa2d14969a7502156c499
Michael Irwin <mikesir87@gmail.com>
2013-06-06 14:21:48 -0400

    Converted tabs to spaces to code style

--- src/main/java/org/jenkinsci/plugins/gcm/im/GcmImConnection.java
+++ src/main/java/org/jenkinsci/plugins/gcm/im/GcmImConnection.java
@@ -16,66 +16,66 @@ import com.google.android.gcm.server.Result;
 
 final class GcmImConnection implements IMConnection {
 
-	private static final GcmImConnection INSTANCE = new GcmImConnection();
+    private static final GcmImConnection INSTANCE = new GcmImConnection();
 
-	static GcmImConnection getInstance() {
-		return INSTANCE;
-	}
+    static GcmImConnection getInstance() {
+        return INSTANCE;
+    }
 
-	@Override
-	public void send(IMMessageTarget target, String text) throws IMException {
-		GcmMessageTarget gcmTarget = (GcmMessageTarget) target;
-		String gcmToken = gcmTarget.getToken();
+    @Override
+    public void send(IMMessageTarget target, String text) throws IMException {
+        GcmMessageTarget gcmTarget = (GcmMessageTarget) target;
+        String gcmToken = gcmTarget.getToken();
 
-		Result result = GcmManager.send(gcmToken, text);
-		checkResult(result, gcmTarget.getUserId());
-	}
+        Result result = GcmManager.send(gcmToken, text);
+        checkResult(result, gcmTarget.getUserId());
+    }
 
-	private void checkResult(Result result, String userId) throws IMException {
-		if (result.getMessageId() == null
-				|| result.getCanonicalRegistrationId() == null)
-			return;
-		GcmUserTokenProperty token = new GcmUserTokenProperty(
-				result.getCanonicalRegistrationId());
-		User user = User.get(userId, false);
-		try {
-			user.addProperty(token);
-			user.save();
-		} catch (IOException e) {
-			throw new IMException(e);
-		}
-	}
+    private void checkResult(Result result, String userId) throws IMException {
+        if (result.getMessageId() == null
+                || result.getCanonicalRegistrationId() == null)
+            return;
+        GcmUserTokenProperty token = new GcmUserTokenProperty(
+                result.getCanonicalRegistrationId());
+        User user = User.get(userId, false);
+        try {
+            user.addProperty(token);
+            user.save();
+        } catch (IOException e) {
+            throw new IMException(e);
+        }
+    }
 
-	@Override
-	public boolean connect() {
-		// Do nothing!
-		return true;
-	}
+    @Override
+    public boolean connect() {
+        // Do nothing!
+        return true;
+    }
 
-	@Override
-	public boolean isConnected() {
-		return true;
-	}
+    @Override
+    public boolean isConnected() {
+        return true;
+    }
 
-	@Override
-	public void setPresence(IMPresence presence, String statusMessage)
-			throws IMException {
-		// Not required
-	}
+    @Override
+    public void setPresence(IMPresence presence, String statusMessage)
+            throws IMException {
+        // Not required
+    }
 
-	@Override
-	public void addConnectionListener(IMConnectionListener listener) {
-		// Not required
-	}
+    @Override
+    public void addConnectionListener(IMConnectionListener listener) {
+        // Not required
+    }
 
-	@Override
-	public void removeConnectionListener(IMConnectionListener listener) {
-		// Not required
-	}
+    @Override
+    public void removeConnectionListener(IMConnectionListener listener) {
+        // Not required
+    }
 
-	@Override
-	public void close() {
-		// Not required
-	}
+    @Override
+    public void close() {
+        // Not required
+    }
 
 }
